a89878035f2c9d5f8902d19410a51dd63f9245bd
Christophe Redwand <credwand@c2r7p7.42perpignan.fr>
2026-02-10 12:12:39 +0100

    first ex

---
+++ ft_garden_intro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+Plant = "Rose"
+Height = int(25)
+Age = int(30)
+
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    print(f"Plant : {Plant}")
+    print(f"Height : {Height}cm")
+    print(f"Age: {Age} days\n")
+    print("=== End of Program ===")
