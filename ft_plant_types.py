2b77021874922fc82e4cb9b17719634698b8488c
red1-chris <red-chris@hotmail.fr>
2026-02-14 16:11:25 +0100

    Add files via upload

--- ft_plant_types.py
+++ ft_plant_types.py
@@ -7,9 +7,34 @@ class Plant:
 
 #Child classes :
 class Flower(Plant):
-    def __init__(self, name, color):
+    def __init__(self, name, color) -> None:
         super().__init__(name)
         self.color = color
 
-    def __str__(self):
-        print(f"{self.name} ({self.class}): {self.height}cm, {self.age} days, {self.color} color")
+    def get_class(self) -> str:
+        self.get_class = "Flower"
+
+    def __str__(self) -> str:
+        print(f"{self.name} ({self.get_class}): {self.height}cm,"
+              f" {self.age} days, {self.color} color")
+        print(f"{self.name} is blooming beautifully!")
+
+class Tree(Plant):
+    def __init__(self, name, trunk_diameter) -> None:
+        super().__init__(name)
+        self.trunk_diameter = trunk_diameter
+
+    def get_class(self) -> None:
+        self.get_class = "Tree"
+
+    def produce_shade(self) -> int:
+        self.produce_shade = 78
+
+    def __str__(self) -> str:
+        print(f"{self.name} ({self.get_class}): {self.height}cm, "
+              f"{self.age} days, {self.trunk_diameter}cm diameter")
+        print(f"{self.name} provides {self.produce_shade} square meters of shade")
+
+class Veetable(Plant):
+    def __init__(self, name, harvest_season):
+        super().__init__()
